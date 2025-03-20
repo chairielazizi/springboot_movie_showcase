1d94504b34da56d756b0c13343047ec706dc4cd6
chairielazizi <azizihr2000@gmail.com>
2025-03-20 15:56:35 +0800

    initialize backend

---
+++ movie_showcase_backend/src/main/java/space/airiel/movie_showcase_backend/MovieShowcaseBackendApplication.java
@@ -0,0 +1,13 @@
+package space.airiel.movie_showcase_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieShowcaseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieShowcaseBackendApplication.class, args);
+	}
+
+}
---
+++ movie_showcase_backend/src/test/java/space/airiel/movie_showcase_backend/MovieShowcaseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package space.airiel.movie_showcase_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieShowcaseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
